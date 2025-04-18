145de1cd71199b7a848a3643318f4ef7e698251f
Caraline mPathic <caralinempathic@mac.lan>
2025-04-18 13:54:55 -0400

    adds revai

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ import json
 import sys
 import time
 
+from dotenv import load_dotenv # Import load_dotenv
+load_dotenv() # Load environment variables from .env file
+
 from src.s3_fh import download_file
 from src.processors import td_revai, td_assemblyai, td_opensource
 # from src.comparison.metrics import calculate_all_metrics
@@ -14,15 +17,41 @@ from src.processors import td_revai, td_assemblyai, td_opensource
 
 
 logging.basicConfig(
-    level=logging.INFO,
+    # level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s %(name)s [%(levelname)s] %(message)s'
 )
 logger = logging.getLogger(__name__)
 
 
+def process_audio(audio_file):
+
+    results = {}
+
+    # do revai
+    logger.info("Starting rev ai processing ...")
+    continuous_transcript, split_diarized_transcript = td_revai.process(audio_file)
+
+    results['revai'] = {
+        'continuous_transcript' : continuous_transcript,
+        'split_diarized_transcript' : split_diarized_transcript
+    }
+
+    # do aai
+    logger.info("Starting assembly ai processing ...")
+
+    # do open source
+
+
 def main():
 
     logger.info("starting ...")
+
+    # run against a single audio file, use something already on disk
+    audio_file = "g2v2sfwfQ84_Brief_intervention_Dave.mp3"
+    process_audio(audio_file)
+
+
     logger.info("Workflow finished.")
 
 
--- src/processors/td_revai.py
+++ src/processors/td_revai.py
@@ -7,11 +7,169 @@ from time import sleep
 
 logger = logging.getLogger(__name__) # respect mains loglevel
 
-REVAI_TOKEN = os.getenv('REVAI_TOKEN', None)
+
+def make_output(transcript):
+	transcript_utterances = []
+
+	for monologue in transcript['monologues']:
+
+		speaker = monologue.get('speaker', 'Unknown Speaker')
+		words = []
+		start_ts = None
+		end_ts = None
+
+		for elem in monologue['elements']:
+			# print(elem)
+
+			if elem['type'] == 'text':
+				words.append(elem['value'])
+				if start_ts is None:
+					start_ts = elem['ts']
+				end_ts = elem['end_ts']
+			else:
+				words.append(elem['value'])
+
+		utterance_text = ''.join(words)
+		string = (f"[{start_ts:.2f} - {end_ts:.2f}] Speaker_{speaker}: {utterance_text}")
+		logger.debug(string)
+
+		transcript_utterances.append({
+			'start_ts' : start_ts,
+			'end_ts' : end_ts,
+			'speaker' : speaker,
+			'text' : utterance_text
+			})
+
+	return transcript_utterances
+
+
+def save_revai_basic_output(transcript_utt, output_path='revai_basic_transcript.txt'):
+	with open(output_path, 'w', encoding='utf-8') as f:
+		for utterance in transcript_utt:
+			speaker = utterance['speaker']
+			text = utterance['text'].strip()
+			start_ts = utterance['start_ts']
+			end_ts = utterance['end_ts']
+
+			string = (f"[{start_ts:.2f} - {end_ts:.2f}] Speaker_{speaker}: {text}")
+			f.write(f"{string}\n")
+
+
+def save_revai_transcript_output(transcript, output_path='revai_trans_transcript.txt'):
+	with open(output_path, 'w', encoding='utf-8') as f:
+		for utterance in transcript:
+			text = utterance['text'].strip()
+			f.write(f"{text} ") # add a space after, its fine
+
+
+def save_revai_diarized_output(transcript, output_path='revai_diarized_transcript.txt'):
+	speaker_texts = {}
+
+	for utt in transcript:
+		if utt['speaker'] not in speaker_texts.keys():
+			speaker_texts[utt['speaker']] = []
+
+		speaker_texts[utt['speaker']].append(utt['text'].strip())
+
+	with open(output_path, 'w', encoding='utf-8') as f:
+		for speaker in speaker_texts.keys():
+			# join their texts together with a space
+			speakers_utterances = ' '.join(speaker_texts[speaker])
+			f.write(f"{speakers_utterances}\n")
+
+
+def write_outputs(transcript_utterances, basename):
+	os.makedirs('out', exist_ok = True)
+	outfile_basic = f"out/out_revai_basic_{basename}.txt"
+	outfile_transcript = f"out/out_revai_transcript_{basename}.txt"
+	outfile_diarized = f"out/out_revai_diarized_group_{basename}.txt"
+
+	save_revai_basic_output(transcript_utterances, outfile_basic)
+	save_revai_transcript_output(transcript_utterances, outfile_transcript)
+	save_revai_diarized_output(transcript_utterances, outfile_diarized)
+
+
+def format_result(transcript):
+	speaker_texts = {}
+	continuous_transcription = []
+
+	for utt in transcript:
+		continuous_transcription.append(utt['text'].strip()) # continuous
+
+		if utt['speaker'] not in speaker_texts.keys():
+			speaker_texts[utt['speaker']] = []
+		speaker_texts[utt['speaker']].append(utt['text'].strip()) # diarized
+
+	# join them into a long string
+	unified_transcript = ' '.join(continuous_transcription)
+
+	what_speakers_said = {}
+	for speaker, textlist in speaker_texts.items():
+		what_speakers_said[speaker] = ' '.join(textlist)
+
+	return unified_transcript, what_speakers_said
+
+
+def callback(transcript, filepath):
+
+	# initial collection of the words
+	transcript_utterances = make_output(transcript) # format the results
+
+	# write the results to disk
+	basename = os.path.basename(filepath)
+	write_outputs(transcript_utterances, basename)
+
+	#  now parse the objects for the return
+	continuous_transcription, diarized_groups = format_result(transcript_utterances)
+
+	logger.debug(f"Continuous transcription: {continuous_transcription}")
+	logger.debug(f"Diarized who said what text:")
+	for k,v in diarized_groups.items():
+		logger.info(f"\t {k}:{v}")
+
+	return continuous_transcription, diarized_groups
+
+
+def process(audio_file):
+
+	# initialize Rev AI API client
+	REVAI_TOKEN = os.environ.get('REVAI_TOKEN')
+	client = apiclient.RevAiAPIClient(REVAI_TOKEN)
+
+	# submit a file for transcription
+	job = client.submit_job_local_file(audio_file)
+
+	# get job id
+	job_id = job.id
+	logger.debug("Job submitted with id: " + job_id)
+
+	# check job status
+	while (job.status.name == 'IN_PROGRESS'):
+
+		details = client.get_job_details(job_id)
+		logger.debug("Job status: " + details.status.name)
+
+		# if successful, print result
+		if (details.status.name == 'TRANSCRIBED'):
+
+			transcript = client.get_transcript_json(job_id)
+			full_transcript, diarized_group_transcript = callback(transcript, audio_file)
+			return full_transcript, diarized_group_transcript
+			break
+
+		# if unsuccessful, print error
+		if (details.status.name == 'FAILED'):
+			logger.error("Rev AI Job failed: " + details.failure_detail)
+			break
+
+		sleep(30)
+
 
 
 if __name__ == '__main__':
 
-	logger.info("transcribing via REV AI ...")
+	logger.debug("REV AI ...")
+
+	# full_transcript, diarized_group_transcript = process(audio_file)
 
 
